c05e7fff7c42be1cc4b56da600846fffd803d6ca
BumboBlast <john.jameschar@gmail.com>
2023-02-19 00:22:57 -0500

    color

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import random
+
 from Space import *
 
 
@@ -18,11 +19,13 @@ class Game:
                 new_button.button.grid(row=rows, column=columns)
                 self.space_list[new_button.position] = new_button
 
+        self.first_click = True
+
     def add_bomb(self, position):
         """ Mark the space in the passed position as a bomb. """
         space = self.space_list[position]
         space.state = space.states[3]
-        space.button.config(state='disabled')
+        space.button.config(bg='red')
 
     def count_bombs(self, position):
         """ returns the amount of bombs in the surrounding 8 spaces. """
@@ -44,16 +47,42 @@ class Game:
             if space[0] in range(0, self.rows) and space[1] in range(0, self.columns):
                 if self.space_list[space].state == Space.states[3]:
                     bombs += 1
+        if bombs == 0:
+            return ''
         return bombs
 
-    def place_random_mines(self, reserved):
+    def place_random_mines(self, start_position):
         # ~ 10 percent are bombs
         # pick 10 percent random spots
-        for r in range(0, int(self.rows * self.columns * 0.1)):
+
+        # reserve the surrounding spaces to the first click
+        reserved = [
+            (start_position[0] - 0, start_position[1] - 0),  # 5, 5
+            (start_position[0] - 0, start_position[1] - 1),  # 5, 4
+            (start_position[0] - 0, start_position[1] + 1),  # 5, 6
+            (start_position[0] - 1, start_position[1] - 0),  # 4, 5
+            (start_position[0] - 1, start_position[1] - 1),  # 4, 4
+            (start_position[0] - 1, start_position[1] + 1),  # 4, 6
+            (start_position[0] + 1, start_position[1] - 0),  # 6, 5
+            (start_position[0] + 1, start_position[1] - 1),  # 6, 4
+            (start_position[0] + 1, start_position[1] + 1),  # 6, 6
+        ]
+
+        # place 10% grid size amount of random bombs, not in reserved spot.
+        for r in range(0, int(self.rows * self.columns * 0.2)):
             sample_x = random.randrange(self.rows)
             sample_y = random.randrange(self.columns)
-            if sample_x not in reserved and sample_y not in reserved:
+            if (sample_x, sample_y) not in reserved:  # should re roll instead
                 self.add_bomb((sample_x, sample_y))
 
-    def get_adjacent_empties(self):
+        # return the list of positions that are not bombs
+        legal_reserved = []
+        for space in reserved:
+            if space[0] in range(0, self.rows) and space[1] in range(0, self.columns):
+                legal_reserved.append(space)
+
+        return legal_reserved
+
+    def get_adjacent_empties(self, start_position):
         """ return a list of adjacent (not diagonal) positions to spaces that are empty. """
+        adjacent_empties = []
--- Space.py
+++ Space.py
@@ -12,13 +12,18 @@ class Space:
         self.button.bind('<Button-3>', lambda event: self.right_click())
         self.button.bind('<Button-1>', lambda event: self.left_click())
         self.button.bind('<Enter>', lambda event: self.sink_button())
-        self.button.bind('<Leave>', lambda event: self.button.config(relief='raised'))
+        self.button.bind('<Leave>', lambda event: self.raise_button())
 
         self.state = self.states[0]
         self.position = (posx, posy)
 
         # address to the game class
         self.game_handle = game
+
+    def raise_button(self):
+        """ Shows the button as raised when cursor leave. Only if empty or bomb. """
+        if self.state == self.states[0] or self.state == self.states[3]:
+            self.button.config(relief='raised')
 
     def sink_button(self):
         """ Shows the button as depressed when mouse over, if empty or a bomb. """
@@ -28,7 +33,7 @@ class Space:
     def right_click(self):
         """ Handles the case the user right-clicks the button. """
         # place a flag
-        if self.state == self.states[0]:
+        if self.state == self.states[0] or self.state == self.states[3]:
             self.state = self.states[1]
             self.button.config(state='disabled')
             self.button.config(text='🚩')
@@ -41,10 +46,28 @@ class Space:
 
     def left_click(self):
         """ Handles the case the user left-clicks the button. """
+
+        # if this is first click of the game
+        if self.game_handle.first_click:
+            self.game_handle.first_click = False
+            empty_spaces = self.game_handle.place_random_mines(self.position)
+            print(empty_spaces)
+
+            # clear some spaces around click
+            for pos in empty_spaces:
+                space = self.game_handle.space_list[pos]
+                space.state = space.states[2]
+                space.button.config(state='disabled')
+                space.button.config(relief='sunken')
+                space.button.config(bg='#e1dad8')
+                space.button.config(text=space.game_handle.count_bombs(pos))
+
         # place a clue if empty
-        if self.state == self.states[0]:
+        elif self.state == self.states[0]:
             self.state = self.states[2]
             self.button.config(state='disabled')
+            self.button.config(relief='sunken')
+            self.button.config(bg='#e1dad8')
             self.button.config(text=self.game_handle.count_bombs(self.position))
 
         # explode if a bomb
